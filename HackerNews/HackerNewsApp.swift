5b2e3f3190af2b96a8f38386c97e5c1108f32cdb
deveshkedia <dkediaphone@icloud.com>
2022-01-03 17:21:58 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Devesh Kedia on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
